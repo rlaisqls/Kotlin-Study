9edc5e7a7a8ce9f795ad96d380a7fcdd68da3d15
rlaisqls <rlaisqls@gmail.com>
2022-10-06 16:47:57 +0900

    📝 :: User

---
+++ kotlkotlin/src/main/kotlin/com/study/kotlkotlin/domain/user/domain/User.kt
@@ -0,0 +1,39 @@
+package com.study.kotlkotlin.domain.user.domain
+
+import com.fasterxml.jackson.annotation.JsonIgnore
+import com.study.kotlkotlin.domain.order.domain.Order
+import com.study.kotlkotlin.domain.user.domain.enums.Authority
+import javax.persistence.*
+
+
+@Entity
+class User(
+    username: String,
+    password: String,
+    authority: Authority
+) {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id", nullable = false)
+    var id: Long = 0
+        protected set
+
+    @Column(nullable = false, unique = true, length = 15)
+    var username: String = username
+        protected set
+
+    @Column(nullable = false, length = 60)
+    var password: String = password
+        protected set
+
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    var authority: Authority = authority
+        protected set
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "user")
+    private val orders: MutableList<Order> = ArrayList<Order>()
+
+}

76e367c511138146dcfd1920f65cb773b32dd0cf
rlaisqls <rlaisqls@gmail.com>
2022-10-31 17:45:39 +0900

    Create: SignInService

---
+++ kotlkotlin/src/main/kotlin/com/study/kotlkotlin/domain/user/service/SignInService.kt
@@ -0,0 +1,29 @@
+package com.study.kotlkotlin.domain.user.service
+
+import com.study.kotlkotlin.domain.auth.presentation.dto.response.TokenResponse
+import com.study.kotlkotlin.domain.user.facade.UserFacade
+import com.study.kotlkotlin.domain.user.presentation.dto.request.SignInRequest
+import com.study.kotlkotlin.global.security.jwt.JwtTokenProvider
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.stereotype.Service
+import javax.transaction.Transactional
+
+@Service
+class SignInService(
+    private val passwordEncoder: PasswordEncoder,
+    private val userFacade: UserFacade,
+    private val jwtTokenProvider: JwtTokenProvider
+) {
+    @Transactional
+    fun execute(request: SignInRequest): TokenResponse {
+
+        val username = request.username
+        val password = request.password
+
+        val user = userFacade.findByUsername(username)
+
+        userFacade.checkPassword(user, password)
+
+        return jwtTokenProvider.generateTokens(username, user.authority)
+    }
+}

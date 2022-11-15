3ae71f64c9a625b952b3d4e20acae28044ca9e65
rlaisqls <rlaisqls@gmail.com>
2022-11-15 19:42:46 +0900

    Create: RegexpUtil

---
+++ kotlkotlin/src/main/kotlin/com/study/kotlkotlin/global/util/RegexpUtil.kt
@@ -0,0 +1,7 @@
+package com.study.kotlkotlin.global.util
+
+object RegexpUtil {
+
+    const val PASSWORD_PATTERN = "(?=.*[a-z])(?=.*[0-9])(?=.*[!#$%&'()*+,./:;<=>?@＼^_`{|}~])[a-zA-Z0-9!#$%&'()*+,./:;<=>?@＼^_`{|}~]{8,30}$"
+
+}
